114ce5dc27efc1fdcd7b1f06cf84de62c4874009
Jim8448 <libra261984@gmail.com>
2026-04-06 10:41:40 -0700

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,3 +4,22 @@ var h1 = document.querySelector(".sitename img");
 window.addEventListener("load", () => {
 h1.classList.add("shimmer");
 });
+
+//brand fade in
+var hero = document.getElementById("heroText");
+var text = hero.innerText;
+var chars = text.split("");
+hero.innerText = "";
+hero.style.opacity = 1;
+
+let i = 0;
+function typeWriter() {
+    if (i < chars.length) {
+        hero.innerHTML += chars[i];
+        i++;
+        setTimeout(typeWriter, 50);
+    } else {
+        hero.style.transform = "translateY(0)";
+    }
+}
+window.onload = typeWriter;
