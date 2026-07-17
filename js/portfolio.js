36f57ffcbb3c9cbed134024874d103bcb8a48c7e
Jim8448 <libra261984@gmail.com>
2026-07-17 14:46:19 -0700

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,25 +1,49 @@
-//h1 on homescreen
-var h1 = document.querySelector(".sitename img");
-
-window.addEventListener("load", () => {
-h1.classList.add("shimmer");
-});
 
 //brand fade in
 var hero = document.getElementById("heroText");
-var text = hero.innerText;
-var chars = text.split("");
-hero.innerText = "";
-hero.style.opacity = 1;
 
-let i = 0;
-function typeWriter() {
-    if (i < chars.length) {
-        hero.innerHTML += chars[i];
-        i++;
-        setTimeout(typeWriter, 50);
-    } else {
-        hero.style.transform = "translateY(0)";
+if (hero) {
+
+    var text = hero.innerText;
+    var chars = text.split("");
+    hero.innerText = "";
+    hero.style.opacity = 1;
+
+    let i = 0;
+
+    function typeWriter() {
+        if (i < chars.length) {
+            hero.innerHTML += chars[i];
+            i++;
+            setTimeout(typeWriter, 50);
+        } else {
+            hero.style.transform = "translateY(0)";
+        }
     }
+
+    window.onload = typeWriter;
 }
-window.onload = typeWriter;
+
+//header slide away
+
+const header = document.querySelector("header");
+
+let previousScroll = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+
+const currentScroll = window.pageYOffset;
+
+if (currentScroll <=0) {
+header.classList.remove("hide");
+previousScroll = currentScroll;
+return;
+}
+if (currentScroll > previousScroll) {
+header.classList.add("hide");
+} else {
+    header.classList.remove("hide");
+}
+
+previousScroll = currentScroll;
+});
